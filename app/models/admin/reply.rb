2a3b03ed6f721b6538ce038aeeaffadaf9346760
Rogers <herenc@isoftstone.com>
2016-12-05 09:36:04 +0800

    bbb

--- app/models/admin/reply.rb
+++ app/models/admin/reply.rb
@@ -1,8 +1,2 @@
 class Admin::Reply < ActiveRecord::Base
-	=begin
-    def reconnectDatabase
-	    logger.warn "@@@@@@@@@@@@reconnectDatabase run" << Time.new.strftime("%Y-%m-%d %H:%M:%S")
-	    establish_connection :production
-	end
-	=end
 end
