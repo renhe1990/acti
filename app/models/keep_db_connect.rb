6077a30204c73af2aac3f2192f986f44b51636f5
Rogers <herenc@isoftstone.com>
2016-12-05 17:17:46 +0800

    vvv

--- app/models/keep_db_connect.rb
+++
@@ -1,6 +0,0 @@
-class KeepDbConnect < ActiveRecord::Base
-    def connectdb
-	    sql = "SELECT 1 FROM DUAL"
-        result = ActiveRecord::Base.connection().execute(sql)
-    end
-end
--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,6 @@
 
 # Example:
 #
- job_type :runner,  "cd :path && rails runner -e :environment ':task' :output"
  set :output, "~/rails_app/shared/log/cron_log.log"
 #
 # every 2.hours do
