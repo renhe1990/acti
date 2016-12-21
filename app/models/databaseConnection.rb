496854f66d0f9750231ab7e909d9a670c9a25d2d
Rogers <herenc@isoftstone.com>
2016-12-21 10:25:53 +0800

    001

--- app/models/databaseConnection.rb
+++ app/models/databaseConnection.rb
@@ -1,18 +1,5 @@
 class DBConn::DatabaseConnection < ActiveRecord::Base
-<<<<<<< .mine
-class DBConn::DatabaseConnection < ActiveRecord::Base
 	self.abstract_class = true
 	#establish_connection DatabaseCnf[:production]
 	establish_connection :production
 end
-
-
-=======
-module DBConn
-	class DatabaseConnection < ActiveRecord::Base
-		self.abstract_class = true
-		#establish_connection DatabaseCnf[:production]
-		establish_connection :production
-	end
-end
->>>>>>> .theirs
