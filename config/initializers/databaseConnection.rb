14e067ad0da50830ea9de628a8e4265092e320a5
Rogers <herenc@isoftstone.com>
2016-12-21 11:22:11 +0800

    004

--- config/initializers/databaseConnection.rb
+++ config/initializers/databaseConnection.rb
@@ -1,5 +1,5 @@
 class DatabaseConnection < ActiveRecord::Base
   self.abstract_class = true
   #establish_connection DatabaseCnf[:production]
-  establish_connection :production
+  establish_connection :development
 end
